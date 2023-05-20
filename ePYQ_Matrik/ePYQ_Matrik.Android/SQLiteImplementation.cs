4e383bf83580918a7069ee88599221fd69d6368e
Brandon2403 <127869157+Brandon2403@users.noreply.github.com>
2023-05-20 22:08:39 +0800

    fixed db issues

--- ePYQ_Matrik/ePYQ_Matrik.Android/SQLiteImplementation.cs
+++ ePYQ_Matrik/ePYQ_Matrik.Android/SQLiteImplementation.cs
@@ -1,17 +1,19 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ePYQ_Matrik.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_Android))]
 
 namespace ePYQ_Matrik.Droid
 {
-    internal class SQLiteImplementation
+    public class SQLite_Android : ISQLite
     {
+        public string GetDatabasePath(string filename)
+        {
+            // Implement the logic to retrieve the database path for Android
+            // For example:
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string databasePath = System.IO.Path.Combine(documentsPath, filename);
+            return databasePath;
+        }
     }
 }

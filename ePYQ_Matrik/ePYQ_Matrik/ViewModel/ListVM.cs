8f7038cfbaa9b639ac4c9d816091e80c6588f903
Brandon2403 <127869157+Brandon2403@users.noreply.github.com>
2023-05-21 23:34:24 +0800

    latest

--- ePYQ_Matrik/ePYQ_Matrik/ViewModel/ListVM.cs
+++ ePYQ_Matrik/ePYQ_Matrik/ViewModel/ListVM.cs
@@ -27,33 +27,31 @@ namespace ePYQ_Matrik.ViewModel
                 {
                     Title = "Biology",
                     Description = "Past Year Question",
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png",
+                    ImageUrl = "https://thumbs.dreamstime.com/b/biology-hand-drawn-doodles-lettering-education-science-vector-white-background-135246167.jpg",
                     PaperUrl = "https://drive.google.com/drive/folders/10NTw_hz73klrdORC9uYyJewv9tqoeRfL?usp=sharing"
                 },
                 new MyItem
                 {
                     Title = "Physics",
                     Description = "Past Year Question",
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png",
+                    ImageUrl = "https://thumbs.dreamstime.com/b/symbols-physics-vector-elements-design-eps-30114404.jpg",
                     PaperUrl = "https://drive.google.com/drive/folders/1WPSFpRFIo6WKWcISpytErcVDxeiaXdIU?usp=sharing"
                 },
                 new MyItem
                 {
                     Title = "Chemistry",
                     Description = "Past Year Question",
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png",
+                    ImageUrl = "https://www.kcl.ac.uk/newimages/nmes/hero-carousel-1903x558/hero-carousel-chemistry-chalk-board-new-Cropped-368x208.jpg",
                     PaperUrl = "https://drive.google.com/drive/folders/1B41i-G2o4180EX3FKGb9F0ljgmVa_qhV?usp=sharing"
                 },
                 new MyItem
                 {
-                    Title = "Math",
+                    Title = "Mathematics",
                     Description = "Past Year Question",
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png",
+                    ImageUrl = "https://s3-ap-south-1.amazonaws.com/ricedigitals3bucket/AUPortalContent/2020/05/08122047/imgmath.jpg",
                     PaperUrl = "https://drive.google.com/drive/folders/1nzBm3ZnGNug4s8ClIMLdJFGz3xrmAMjQ?usp=sharing"
                 },
                
-             
-                  
                 // Add more items as needed
             };
         }
